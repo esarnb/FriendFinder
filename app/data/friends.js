886400d71efbf6c3218d398b5fa81add1073bda0
Esar Behlum <esarnb@gmail.com>
2019-06-29 03:50:53 -0700

    Add data transfer and unique-id

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,3 @@
-var friends = [{
-  name: "Esar",
-  photo: "https://banner2.kisspng.com/20180730/hri/kisspng-baby-penguins-infant-cuteness-kowalsky-penguin-5b5ecf625f0207.3795252015329401303892.jpg",
-  survey: ["1", "2", "3", "4", "5", "4", "3", "2", "1"]
-}]
+var friends = []
 
 module.exports = friends;
