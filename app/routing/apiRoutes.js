21b2bacb3504f9fa86fffec3d259bc56d7bf306a
Esar Behlum <esarnb@gmail.com>
2019-06-29 02:05:57 -0700

    Working Routes Commit: Color pallet change, added materialize forms/sliders/buttons.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,23 @@
+var friends = require("../data/friends");
 
 module.exports = function(app) {
 
-  //
+  /**
+   * Used to display a JSON of all possible friends
+   */
+  app.get("/api/friends", function(req, res) {
+    console.log(friends);
+    
+    res.json(friends)
+  })
+
+  /**
+   * Used to handle incoming survey results 
+   * and handles compatibility logic.
+   */
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body)
+    friends.push(req.body);
+  })
 
 }
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 7500;
 
+//Static for CSS Stylesheet compatibility
+app.use(express.static('app'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
