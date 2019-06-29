2532d1d5ef4ab854731cd6696d66a8101b6d8657
Esar Behlum <esarnb@gmail.com>
2019-06-29 12:34:00 -0700

    Add starter data, add unique-id for profiles (extra feature), add apiRoutes match calculations

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,16 @@
-var friends = []
+var friends = [
+  {
+    name: "Esar",
+    photo: "No Photo",
+    survey: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    FFuuid: "123"
+  },
+  {
+    name: "Penguino",
+    photo: "No Photo",
+    survey: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    FFuuid: "333"
+  }
+]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,6 @@ module.exports = function(app) {
    * Used to display a JSON of all possible friends
    */
   app.get("/api/friends", function(req, res) {
-    console.log(friends);
-    
     res.json(friends)
   })
 
@@ -16,8 +14,31 @@ module.exports = function(app) {
    * and handles compatibility logic.
    */
   app.post("/api/friends", function(req, res) {
-    console.log(req.body)
+    req.body.survey = req.body.survey.map(el => parseInt(el));
     friends.push(req.body);
+    
+    var totals = [], lowestSum = -1;
+    //For each friend in db...
+    for(perFriend of friends) {
+      //Do not make current member a possible choice for a match
+      if (perFriend.FFuuid !== req.body.FFuuid) {
+        var currentSum = 0;
+        //Sum the differences for each answer
+        for(var i = 0; i < perFriend.survey.length; i++) {
+            currentSum += Math.abs(req.body.survey[i] - perFriend.survey[i]);
+        }
+        //Store each diff into totals with friend's id.
+        totals.push({FFuuid: perFriend.FFuuid, sum: currentSum, name: perFriend.name, photo: perFriend.photo});
+      }
+    }
+    //Find lowest diff
+    totals.sort((a,b) => (a.sum > b.sum) ? 1 : -1);
+    
+   var filterSum = totals[0].sum;
+   var matches = totals.filter(el => el.sum == filterSum).map(el => `Name: ${el.name}\nPhoto: ${el.photo}\n`).join("\n")
+   console.log("===",matches,"===");
+   
+    return res.json(matches);
   })
 
 }
