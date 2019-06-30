ea23b845939e9cbcb6eda9c6415cc179827cd084
Esar Behlum <esarnb@gmail.com>
2019-06-29 23:46:14 -0700

    Removed FFuuid, removed profile btn, Returned potential matches in rows&cols within survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,13 @@
 var friends = [
   {
     name: "Esar",
-    photo: "No Photo",
-    survey: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    FFuuid: "123"
+    photo: "https://pbs.twimg.com/profile_images/961238719268257792/QAE3SnEi_400x400.jpg",
+    survey: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   },
   {
     name: "Penguino",
-    photo: "No Photo",
-    survey: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-    FFuuid: "333"
+    photo: "http://localhost:7500/images/web/logo.png",
+    survey: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,28 +17,33 @@ module.exports = function(app) {
     req.body.survey = req.body.survey.map(el => parseInt(el));
     friends.push(req.body);
     
-    var totals = [], lowestSum = -1;
+    var totals = [];
     //For each friend in db...
     for(perFriend of friends) {
-      //Do not make current member a possible choice for a match
-      if (perFriend.FFuuid !== req.body.FFuuid) {
+      
+      /*
+        Do not make current member a possible choice for 
+        a match with his/her self for this instance only.
+      */
+      if (perFriend.name !== req.body.name) {
         var currentSum = 0;
         //Sum the differences for each answer
         for(var i = 0; i < perFriend.survey.length; i++) {
             currentSum += Math.abs(req.body.survey[i] - perFriend.survey[i]);
         }
         //Store each diff into totals with friend's id.
-        totals.push({FFuuid: perFriend.FFuuid, sum: currentSum, name: perFriend.name, photo: perFriend.photo});
+        totals.push({sum: currentSum, name: perFriend.name, photo: perFriend.photo});
       }
     }
     //Find lowest diff
     totals.sort((a,b) => (a.sum > b.sum) ? 1 : -1);
-    
+  
+   //Get all users with the same match diff compatibility
    var filterSum = totals[0].sum;
-   var matches = totals.filter(el => el.sum == filterSum).map(el => `Name: ${el.name}\nPhoto: ${el.photo}\n`).join("\n")
-   console.log("===",matches,"===");
+   var matches = totals.filter(el => el.sum == filterSum);
    
-    return res.json(matches);
+   //Send data back to client about potential matches
+   matches ? res.json(matches) : res.json({"Error":"No Matches"})
   })
 
 }
